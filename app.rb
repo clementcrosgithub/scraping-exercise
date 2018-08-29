0a788e3b28074894fe0236cc5bbf7e9bd17a94e6
clement cros <clementcros@hotmail.fr>
2018-08-29 19:53:13 +0100

    update1

--- app.rb
+++ app.rb
@@ -34,18 +34,4 @@ end
   end
 end
 
-# describe '#destroy' do
 
-#   it 'when resource is found' do
-#     it 'responds with 200'
-#     it 'shows the resource'
-#   end
-
-#   context 'when resource is not found' do
-#     it 'responds with 404'
-#   end
-
-#   context 'when resource is not owned' do
-#     it 'responds with 404'
-#   end
-end
